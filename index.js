fb26297cc2b294c8ef45556570a03957e0fac1cb
fardog <nate@milkandtang.com>
2015-08-20 16:34:23 -0700

    Ignore query parameters when routing, standard style

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
+var url = require('url')
+
 var Routes = require('routes')
-  , extend = require('deep-extend')
+var extend = require('deep-extend')
 
 module.exports = createRouter
 
-function createRouter(defaultRoute, _root, _verbs) {
+function createRouter (defaultRoute, _root, _verbs) {
   var routers = {}
-    , root = _root || ''
-    , verbs = _verbs || ['get', 'post', 'put', 'patch', 'delete']
-    , routeFn
+  var root = _root || ''
+  var verbs = _verbs || ['get', 'post', 'put', 'patch', 'delete']
+  var routeFn
 
   verbs.push('any')
 
-  routeFn = function() {
+  routeFn = function () {
     var args = [].slice.call(arguments)
-      , req = args[0]
-      , route = req.splats && req.splats.length ?
-          req.splats[req.splats.length - 1] :
-          req.url
-      , method = req.method ? req.method.toLowerCase() : 'any'
-      , result
+    var req = args[0]
+    var route = req.splats && req.splats.length ?
+      req.splats[req.splats.length - 1] :
+      url.parse(req.url).pathname
+    var method = req.method ? req.method.toLowerCase() : 'any'
+    var result
 
     if (root.length && route.indexOf(root) === 0) {
       route = route.slice(root.length, route.length)
     }
 
-    if(routers[method]) {
+    if (routers[method]) {
       result = routers[method].match(route)
     }
 
-    if(!result) {
+    if (!result) {
       result = routers.any.match(route)
     }
 
-    if(result) {
+    if (result) {
       args[0] = extend(req, result)
       result.fn.apply(null, args)
     } else {
-      if(typeof defaultRoute === 'function') {
+      if (typeof defaultRoute === 'function') {
         defaultRoute.apply(null, args)
 
         return
@@ -46,9 +48,9 @@ function createRouter(defaultRoute, _root, _verbs) {
     }
   }
 
-  verbs.forEach(function(verb) {
+  verbs.forEach(function (verb) {
     routers[verb] = Routes()
-    routeFn[verb] = function(pattern, handler) {
+    routeFn[verb] = function (pattern, handler) {
       routers[verb].addRoute(pattern, handler)
     }
   })
--- test.js
+++ test.js
@@ -1,36 +1,53 @@
 var test = require('tape')
-  , commuter = require('./index')
+var commuter = require('./index')
 
-test('routes on a request-like object', function(t) {
+test('routes on a request-like object', function (t) {
   t.plan(1)
 
   var router = commuter()
-    , req = {url: '/path', method: 'GET'}
+  var req = {url: '/path', method: 'GET'}
 
   router.get(req.url, onRoute)
 
   router(req)
 
-  function onRoute() {
+  function onRoute () {
     t.ok(true)
 
     t.end()
   }
 })
 
-test('passes through any additional parameters provided', function(t) {
+test('routes on a querystring path', function (t) {
+  t.plan(1)
+
+  var router = commuter()
+  var req = {url: '/path?something=true', method: 'GET'}
+
+  router.get('/path', onRoute)
+
+  router(req)
+
+  function onRoute () {
+    t.ok(true)
+
+    t.end()
+  }
+})
+
+test('passes through any additional parameters provided', function (t) {
   t.plan(2)
 
   var router = commuter()
-    , req = {url: '/path', method: 'GET'}
-    , res = {ok: function() {}}
-    , superflous = {}
+  var req = {url: '/path', method: 'GET'}
+  var res = {ok: function () {}}
+  var superflous = {}
 
   router.get(req.url, onRoute)
 
   router(req, res, superflous)
 
-  function onRoute(request, response, unnecessary) {
+  function onRoute (request, response, unnecessary) {
     t.strictEqual(res, response)
     t.strictEqual(superflous, unnecessary)
 
@@ -38,28 +55,28 @@ test('passes through any additional parameters provided', function(t) {
   }
 })
 
-test('calls error function on no match', function(t) {
+test('calls error function on no match', function (t) {
   t.plan(1)
 
   var router = commuter(onError)
-    , url = '/path'
-    , req = {url: '/boop', method: 'GET'}
+  var url = '/path'
+  var req = {url: '/boop', method: 'GET'}
 
   router.get(url, noop)
 
   router(req)
 
-  function onError(r) {
+  function onError (r) {
     t.strictEqual(r, req)
   }
 })
 
-test('throws error if no error function provided', function(t) {
+test('throws error if no error function provided', function (t) {
   t.plan(1)
 
   var router = commuter()
-    , url = '/path'
-    , req = {url: '/boop', method: 'GET'}
+  var url = '/path'
+  var req = {url: '/boop', method: 'GET'}
 
   router.get(url, noop)
 
@@ -72,38 +89,38 @@ test('throws error if no error function provided', function(t) {
   }
 })
 
-test('places parameters on the request object', function(t) {
+test('places parameters on the request object', function (t) {
   t.plan(1)
 
   var router = commuter()
-    , url = '/admin/:panel'
-    , req = {url: '/admin/home', method: 'GET'}
+  var url = '/admin/:panel'
+  var req = {url: '/admin/home', method: 'GET'}
 
   router.get(url, onRoute)
 
   router(req)
 
-  function onRoute(r) {
+  function onRoute (r) {
     t.equal('home', r.params.panel)
 
     t.end()
   }
 })
 
-test('routes on expected sub-routes', function(t) {
+test('routes on expected sub-routes', function (t) {
   t.plan(2)
 
   var router = commuter()
-    , subRouter = commuter()
-    , req = {url: '/admin/section/home', method: 'GET'}
-    , res = {ok: function() {}}
+  var subRouter = commuter()
+  var req = {url: '/admin/section/home', method: 'GET'}
+  var res = {ok: function () {}}
 
   router.get('/admin*', subRouter)
   subRouter.get('/section/:panel', onRoute)
 
   router(req, res)
 
-  function onRoute(request, response) {
+  function onRoute (request, response) {
     t.equal(request.params.panel, 'home')
     t.strictEqual(response, res)
 
@@ -111,37 +128,58 @@ test('routes on expected sub-routes', function(t) {
   }
 })
 
-test('strips root from url when provided', function(t) {
+test('routes on expected sub-routes with query params', function (t) {
+  t.plan(2)
+
+  var router = commuter()
+  var subRouter = commuter()
+  var req = {url: '/admin/section/home?something=true', method: 'GET'}
+  var res = {ok: function () {}}
+
+  router.get('/admin*', subRouter)
+  subRouter.get('/section/:panel', onRoute)
+
+  router(req, res)
+
+  function onRoute (request, response) {
+    t.equal(request.params.panel, 'home')
+    t.strictEqual(response, res)
+
+    t.end()
+  }
+})
+
+test('strips root from url when provided', function (t) {
   t.plan(1)
 
   var router = commuter(null, '/strip')
-    , req = {url: '/strip/admin/section/home', method: 'GET'}
+  var req = {url: '/strip/admin/section/home', method: 'GET'}
 
   router.get('/admin*', onRoute)
 
   router(req)
 
-  function onRoute(request) {
+  function onRoute (request) {
     t.ok(request)
 
     t.end()
   }
 })
 
-test('root url does not affect sub-routes', function(t) {
+test('root url does not affect sub-routes', function (t) {
   t.plan(2)
 
   var router = commuter(null, '/strip')
-    , subRouter = commuter()
-    , req = {url: '/strip/admin/section/home', method: 'GET'}
-    , res = {ok: function() {}}
+  var subRouter = commuter()
+  var req = {url: '/strip/admin/section/home', method: 'GET'}
+  var res = {ok: function () {}}
 
   router.get('/admin*', subRouter)
   subRouter.get('/section/:panel', onRoute)
 
   router(req, res)
 
-  function onRoute(request, response) {
+  function onRoute (request, response) {
     t.equal(request.params.panel, 'home')
     t.strictEqual(response, res)
 
@@ -149,54 +187,54 @@ test('root url does not affect sub-routes', function(t) {
   }
 })
 
-test('arbitrary verbs can be defined', function(t) {
+test('arbitrary verbs can be defined', function (t) {
   t.plan(1)
 
   var router = commuter(null, null, ['leap'])
-    , req = {url: '/path', method: 'leap'}
+  var req = {url: '/path', method: 'leap'}
 
   router.leap(req.url, onRoute)
 
   router(req)
 
-  function onRoute() {
+  function onRoute () {
     t.ok(true)
 
     t.end()
   }
 })
 
-test('"any" route is always present', function(t) {
+test('"any" route is always present', function (t) {
   t.plan(1)
 
   var router = commuter(null, null, ['leap'])
-    , req = {url: '/path', method: 'wut'}
+  var req = {url: '/path', method: 'wut'}
 
   router.any(req.url, onRoute)
 
   router(req)
 
-  function onRoute() {
+  function onRoute () {
     t.ok(true)
 
     t.end()
   }
 })
 
-test('latest defined parameter takes precedence', function(t) {
+test('latest defined parameter takes precedence', function (t) {
   t.plan(2)
 
   var router = commuter()
-    , subRouter = commuter()
-    , req = {url: '/admin/item/go/section/home', method: 'GET'}
-    , res = {ok: function() {}}
+  var subRouter = commuter()
+  var req = {url: '/admin/item/go/section/home', method: 'GET'}
+  var res = {ok: function () {}}
 
   router.get('/admin/:panel/:section/*', subRouter)
   subRouter.get('section/:section', onRoute)
 
   router(req, res)
 
-  function onRoute(request, response) {
+  function onRoute (request, response) {
     t.equal(request.params.panel, 'item')
     t.equal(request.params.section, 'home')
 
@@ -204,6 +242,6 @@ test('latest defined parameter takes precedence', function(t) {
   }
 })
 
-function noop() {
+function noop () {
   //
 }
